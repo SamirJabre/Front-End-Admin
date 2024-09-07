576fbaacc38146a7e3567c8452e33c7dda2fccfc
Samir Jabre <73381924>
2024-09-07 19:19:53 +0300

    Creating the dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Login from './pages/LoginPage/login';
 
-function App() {
 
+function App() {
+  const [adminInfo,setAdminInfo] = useState({})
   return (
     <BrowserRouter>
       <Routes>  
-        <Route index element={<Login/>} />
+        <Route index element={<Login setAdminInfo={setAdminInfo}/>} />
+        <Route path='/register' element={<RegisterPage setUserInfo={setUserInfo}/>} />
       </Routes>
     </BrowserRouter>
   )
