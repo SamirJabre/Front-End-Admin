47172512193b6c54784d25779dc244aee0e96a25
Samir Jabre <73381924>
2024-09-07 19:03:49 +0300

    Making the folders and organizing them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    
+    <h1>
+      This is the home page
+    </h1>
   )
 }
 
