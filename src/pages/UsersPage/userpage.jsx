8732644967ce93fff3129577bec418b97a52dfd7
Samir Jabre <73381924>
2024-09-08 07:42:02 +0300

    Finished the userPage

--- src/pages/UsersPage/userpage.jsx
+++ src/pages/UsersPage/userpage.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import './userpage.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './userpage.css';
 
 function userpage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+    axios.get(`${BASE_URL}/getusers`)
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users!', error);
+      });
+  }, []);
+
+  const deleteUser = (userId) => {
+    // Delete user by ID
+    axios.delete(`${BASEURL}/${userId}`)
+      .then(() => {
+        // Update the state to remove the deleted user
+        setUsers(users.filter(user => user.id !== userId));
+      })
+      .catch(error => {
+        console.error('There was an error deleting the user!', error);
+      });
+  };
+
   return (
-    <div>userpage</div>
-  )
+    <div className="user-page">
+      <h1>User Page</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} <button onClick={() => deleteUser(user.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default userpage
+export default userpage;
