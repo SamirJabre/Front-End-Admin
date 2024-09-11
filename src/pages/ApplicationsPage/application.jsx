5be3287f6756b730099c33e9aed6e47fbf670cf1
Samir Jabre <73381924>
2024-09-11 13:35:46 +0300

    Made the applicationContent as a component

--- src/pages/ApplicationsPage/application.jsx
+++ src/pages/ApplicationsPage/application.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../components/NavBar/navbar'
 import './application.css'
 import ApplicationContent from '../../components/ApplicationContent/applicationContent'
+import axios from 'axios';
 
 function application() {
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+  const [applications,setApplications] = useState([])
+
+  useEffect(()=>{
+    axios.get(`${BASE_URL}/get-apps`)
+    .then((res)=>
+      setApplications(res.data))
+  },[])
   return (
     <div className='applicationPage'>
     <NavBar/>
     <div className='applicationContainer'>
-    <ApplicationContent/>
-    <ApplicationContent/>
-    <ApplicationContent/>
-    <ApplicationContent/>
-    <ApplicationContent/>
+      {applications.map((application)=>{
+        return <ApplicationContent key={application.id} application={application}/>
+      })
+      }
     </div>
     </div>
   )
