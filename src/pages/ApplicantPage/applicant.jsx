7c24553c9767bf5f237492d01be374f7fa3b08c2
Samir Jabre <73381924>
2024-09-11 12:59:26 +0300

    Fixing Bugs

---
+++ src/pages/ApplicantPage/applicant.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function applicant() {
+  return (
+    <div>applicant</div>
+  )
+}
+
+export default applicant
--- src/pages/ApplicationsPage/application.jsx
+++ src/pages/ApplicationsPage/application.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/navbar'
 import './application.css'
+import {useNavigate} from 'react-router-dom'
 
 function application() {
+    let navigate = useNavigate();
   return (
     <div className='applicationPage'>
     <NavBar/>
     <div className='applicationContainer'>
-        <div className='applicationContent'>
+        <div className='applicationContent' onClick={()=>{navigate('/application')}}>
             <img src='https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg' id='driver_img'/>
             <div id='driver_info'>
             <h1 id='driver_name'>Name : Nabil</h1>
