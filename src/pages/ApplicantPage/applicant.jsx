fdeae78aab02fa13054322a8a26dcd88dc819382
Samir Jabre <73381924>
2024-09-11 15:40:27 +0300

    Handling approving the applications is done

--- src/pages/ApplicantPage/applicant.jsx
+++ src/pages/ApplicantPage/applicant.jsx
@@ -1,17 +1,42 @@
 import React, { useEffect } from 'react'
 import './applicant.css'
 import NavBar from '../../components/NavBar/navbar'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 function applicant() {
-
+  let navigate = useNavigate();
+  const BASE_URL = import.meta.env.VITE_BASE_URL;
   const location = useLocation();
   const { application } = location.state || {};
 
 
   console.log(application);
   
+  const handleApprove = async () => {
+    try{
+      await axios.post(`${BASE_URL}/approve-app`, {
+        id: application.id,
+        name: application.name,
+        email: application.email,
+        password: application.password,
+        phone_number: application.phone_number,
+        profile_picture: application.profile_picture,
+        address: application.address,
+        id_photo: application.id_photo,
+        driver_license: application.driver_license,
+        })
+        .then((res) => navigate('/drivers-applications') )
+    }
+    catch (error) {
+      console.error('Error approving application:', error);
+    }
+  }
+
+
+  
+
 
   return (
     <div className='applicantPageContainer'>
@@ -34,8 +59,8 @@ function applicant() {
     </div>
 
     <div className='btns'>
-        <button id='reject'>Reject</button>
-        <button id='accept'>Accept</button>
+        <button id='reject' onClick={handleReject}>Reject</button>
+        <button id='accept' onClick={handleApprove}>Accept</button>
     </div>
 
     </div>
