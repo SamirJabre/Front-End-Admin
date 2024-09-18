a3dc5d93d28224deff0d6232221e009105876c3c
Samir Jabre <73381924>
2024-09-18 13:04:57 +0300

    Applied Fixes

--- src/components/ApplicationContent/applicationContent.jsx
+++ src/components/ApplicationContent/applicationContent.jsx
@@ -4,17 +4,21 @@ import {useNavigate} from 'react-router-dom'
 
 function applicationContent({application}) {
   const [name,setName] = useState('')
+  const [age,setAge] = useState('')
+  const [profilePic,setProfilePic] = useState('')
   useEffect(()=>{
     setName(application.name)
-  },[name])
+    setAge(application.age)
+    setProfilePic(application.profile_picture)
+  },[profilePic])
   
     let navigate = useNavigate();
   return (
     <div className='applicationContent' onClick={()=>{navigate('/application', { state: { application } })}}>
-            <img src='https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg' id='driver_img'/>
+            <img src={profilePic} id='driver_img'/>
             <div id='driver_info'>
             <h1 id='driver_name'>Name : {name}</h1>
-            <h1 id='driver_age'>Age : 30</h1>
+            <h1 id='driver_age'>Age : {age}</h1>
             </div>
         </div>
   )
--- src/pages/ApplicantPage/applicant.jsx
+++ src/pages/ApplicantPage/applicant.jsx
@@ -52,7 +52,7 @@ function applicant() {
     <div className='applicantPageContainer'>
     <NavBar/>
     <div className='img_container'>
-        <img src='https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg' id='img_driver'/>
+        <img src={application.profile_picture} id='img_driver'/>
     </div>
 
     <div className='applicant_info'>
